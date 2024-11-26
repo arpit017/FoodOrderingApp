94b66ae9d835a56d096d166eac9e64fdcd186c3e
arpit017 <shuklaji9706@gmail.com>
2024-11-26 11:04:19 +0530

    1.Building UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,42 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet,Text,View,Image } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import products from '../../assets/data/products';
+import Colors from '@/constants/Colors';
+const product = products[0];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <Image
+        style={styles.image}
+        source={{uri: product.image}}
+      />
+
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:'white',
+    padding:10,
+    borderRadius:10
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginVertical:10
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  price:{
+    fontSize:15,
+    color:Colors.light.tint,
+    fontWeight:'bold'
   },
+  image:{
+    width:'100%',
+    aspectRatio:1
+  }
 });
