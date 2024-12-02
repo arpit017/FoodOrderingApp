3055bad57b9d0692340d90497062ab5778dae85c
arpit017 <shuklaji9706@gmail.com>
2024-12-02 22:55:16 +0530

    Shopping Cart

--- app/cart.tsx
+++ app/cart.tsx
@@ -3,20 +3,23 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { useCart } from './CartProvider'
 import CartListItem from '@/components/CartListItem'
+import Button from '@/components/Button'
 
 
 const cartScreen = () => {
 
-const {items} = useCart()
+const {items,total} = useCart()
 
   return (
-    <View>
+    <View style={{padding:10}}>
       <FlatList 
       data={items}
       renderItem={({item})=><CartListItem cartItem={item}/>}
-      contentContainerStyle={{padding:10,gap:10}}
+      contentContainerStyle={{gap:10}}
       />
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      <Text style={{paddingVertical:5,fontSize:16}}>Total:{total.toFixed(2)}</Text>
+      <Button text={"Checkout"} />
     </View>
   )
 }
